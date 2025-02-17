5fc6e55c3cd703fb37ff80291a5d5968c09ab64a
Aditya <adityarastogi620@gmail.com>
2025-02-18 01:29:17 +0530

    inital commit

---
+++ src/main/java/com/adityarastogi/lumiJournal/LumiJournalApplication.java
@@ -0,0 +1,13 @@
+package com.adityarastogi.lumiJournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiJournalApplication.class, args);
+	}
+
+}
