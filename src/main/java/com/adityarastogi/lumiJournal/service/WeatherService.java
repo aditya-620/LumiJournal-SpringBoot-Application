cc6668b29ddf4c5355244129017cb898173c904b
Aditya <adityarastogi620@gmail.com>
2025-04-05 03:12:07 +0530

    Added Redis features

--- src/main/java/com/adityarastogi/lumiJournal/service/WeatherService.java
+++ src/main/java/com/adityarastogi/lumiJournal/service/WeatherService.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 
 import com.adityarastogi.lumiJournal.apiResponse.WeatherResponse;
 import com.adityarastogi.lumiJournal.cache.AppCache;
-import com.adityarastogi.lumiJournal.constants.PlaceHolders;
 
 
 @Service
@@ -24,10 +23,30 @@ public class WeatherService {
     @Autowired
     private RestTemplate restTemplate; // It is a class in a spring framework, which process HTTPS request and give response
 
+    @Autowired
+    private RedisService redisService;
+
+    // public WeatherResponse getWeather(String city){
+    //     String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace("<city>", city).replace("<apiKey>", apiKey);
+    //     ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+    //     WeatherResponse body = response.getBody();  
+    //     return body;
+    // }
+
     public WeatherResponse getWeather(String city){
-        String finalAPI = appCache.appCache.get(AppCache.keys.WEATHER_API.toString()).replace(PlaceHolders.CITY, city).replace(PlaceHolders.API_KEY, apiKey);
-        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
-        WeatherResponse body = response.getBody();  
-        return body;
+        WeatherResponse weatherResponse = redisService.get("weather_of_" + city, WeatherResponse.class);
+        if(weatherResponse != null){
+            return weatherResponse;
+        }
+
+        else{
+            String finalAPI = appCache.appCache.get("weather_api").replace("<city>", city).replace("<apiKey>", apiKey);
+            ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalAPI, HttpMethod.GET, null, WeatherResponse.class);
+            WeatherResponse body = response.getBody();
+            if(body!=null){
+                redisService.set("weather_of_" + city, body, 300L);
+            }
+            return body;
+        } 
     }
 }
---
+++ src/test/java/com/adityarastogi/lumiJournal/service/RedisTests.java
@@ -0,0 +1,21 @@
+// package com.adityarastogi.lumiJournal.service;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.data.redis.core.RedisTemplate;   
+
+// @SpringBootTest
+// public class RedisTests {
+
+//     @Autowired
+//     private RedisTemplate<String, Object> redisTemplate;
+    
+//     @Test
+//     void testSendMail() {
+//         redisTemplate.opsForValue().set("email", "aditya@gmail.com");
+
+//         Object email = redisTemplate.opsForValue().get("email");
+//         System.out.println("Retrieved email: " + email);
+//     }
+// }
