56cddde49d03a24cc37add152aa9a93ff56fad0c
Aditya <adityarastogi620@gmail.com>
2025-07-09 02:22:16 +0530

    email service

--- src/main/java/com/adityarastogi/lumiJournal/controller/PublicController.java
+++ src/main/java/com/adityarastogi/lumiJournal/controller/PublicController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -71,6 +70,11 @@ public class PublicController {
         newUser.setEmail(user.getEmail());
         newUser.setSentimentAnalysis(user.isSentimentAnalysis());
         userService.saveNewUser(newUser);
+
+        emailService.sendEmail(user.getEmail(), "Hi, " + user.getUserName(), "Welcome to Lumi Journal! \n\n" +
+                "We are excited to have you on board. \n\n" +
+                "Best regards, \n" +
+                "Lumi Journal Team");
     }
 
     @PostMapping("/login")
